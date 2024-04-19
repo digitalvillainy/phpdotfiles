dbe1b4712ce047956f99581a86fd6ad9dcd43996
rrivera <rrivera@redbannermedia.com>
2024-04-18 23:53:28 -0400

    initial commit

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,15 @@
+return {
+  "olrtg/emmet-language-server",
+  filetypes = {
+    "css",
+    "html",
+    "php",
+    "javascript",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "vue",
+  },
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua", -- Used to format lua code
+      "intelephense",
+      "vue-language-server",
+      "php-cs-fixer",
+      "lua-language-server",
+      "blade-formatter",
+      "css-lsp",
+      "emmet-language-server",
+      "json-lsp",
+      "marksman",
+      "tailwindcss-language-server",
+      "typescript-language-server",
+      "pyright",
+      "pint",
+      "html-lsp",
+    },
+  },
+}
